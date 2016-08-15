5d43318645a4bb7f5c1286ecea49cbcc55a78fae
Shrimadhav U K <shrimadhavuk@gmail.com>
2016-08-15 23:19:41 +0530

    a small suggestion

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$('#fullpage').fullpage({
+    // sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+    anchors: ['1page', '2page', '3page', '4page', '5page', '6page', '7page'],
+    menu: '#menu',
+    css3: true,
+    scrollingSpeed: 1000
+});
