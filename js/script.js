fda82bbb64a3bc13d705f0dafa399ebf24808198
Shrimadhav U K <shrimadhavuk@gmail.com>
2016-08-19 07:03:29 +0530

    fix NAV issue

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $('#fullpage').fullpage({
     // sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-    anchors: ['1page', '2page', '3page', '4page', '5page', '6page', '7page'],
+    // anchors: ['1page', '2page', '3page', '4page', '5page', '6page', '7page'],
     menu: '#menu',
-    css3: true,
+    css3: false,
     scrollingSpeed: 1000
 });
