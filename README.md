192239ac85b8e575b7b047714a641b8f8d9d936b
Yash Agarwal <yashagarwaljpr@gmail.com>
2016-09-02 17:34:13 +0530

    anchor links working

--- README.md
+++
@@ -1,4 +0,0 @@
-# fossmeet17
-Website for FOSSMeet '17
-
-This is the repository which will contain the source code of FOSSMEET '17 website. 
--- js/script.js
+++ js/script.js
@@ -1,16 +1,66 @@
-$('#container').XSwitch({
-    selectors: {
-        sections: '.sections',
-        section: '.section',
-        page: '.pages',
-        active: '.active'
-    },
-    index: 0,
-    easing: 'ease',
-    duration: 700,
-    loop: false,
-    pagination: true,
-    keyboard: true,
-    direction: 'vertical',
-    callback: ''
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: '#menu',
+        lockAnchors: false,
+        anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        showActiveTooltip: true,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        interlockedSlides: false,
+        resetSliders: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        sectionsColor : ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        fixedElements: '#header, .footer',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+        responsiveSlides: false,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+    });
 });
