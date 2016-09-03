ef6902e9d2fdaf5483e00ed5cdd9b229d417feed
Yash Agarwal <yash2696@users.noreply.github.com>
2016-09-03 23:07:47 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         //Navigation
         menu: '#menu',
         lockAnchors: false,
-        anchors:['main', 'About', 'Events', 'Speakers', 'Gallary', 'Location', 'Contact', 'contactForm', 'close'],
+        anchors:['main', 'About', 'Events', 'Speakers', 'Gallary', 'Location', 'Contact', 'ContactForm', 'Close'],
         navigation: false,
         navigationPosition: 'right',
         showActiveTooltip: true,
