f11236faa51ed9a3320154c82f7022ca87c62622
Yash Agarwal <yashagarwaljpr@gmail.com>
2016-09-10 21:15:44 +0530

    somewhat responsive

--- js/script.js
+++ js/script.js
@@ -1,128 +1,78 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage({
-        //Navigation
-        menu: '#menu',
-        lockAnchors: true,
-        anchors:['Main', 'About', 'Events', 'Speakers', 'Sponsors', 'Location'],
-        navigation: true,
-        navigationPosition: 'right',
-        showActiveTooltip: true,
-        slidesNavigation: true,
-        slidesNavPosition: 'bottom',
-        fadingEffect:true,
-        faddingEffect:'sections',
-        
-        //Scrolling
-        continuousHorizontal:true,
-        scrollDelay: 2000,
-        css3: true,
-        scrollingSpeed: 900,
-        autoScrolling: true,
-        fitToSection: true,
-        fitToSectionDelay: 4000,
-        scrollBar: false,
-        easing: 'easeInOutCubic',
-        easingcss3: 'ease',
-        loopBottom: false,
-        loopTop: false,
-        loopHorizontal: true,
-        continuousVertical: false,
-        continuousHorizontal: false,
-        scrollHorizontally: false,
-        interlockedSlides: false,
-        resetSliders: false,
-        normalScrollElements: '#element1, .element2',
-        scrollOverflow: false,
-        scrollOverflowOptions: null,
-        touchSensitivity: 15,
-        normalScrollElementTouchThreshold: 5,
-        bigSectionsDestination: null,
-
-        //Accessibility
-        keyboardScrolling: true,
-        animateAnchor: true,
-        recordHistory: true,
-
-        //Design
-        controlArrows: true,
-        verticalCentered: true,
-//        sectionsColor : ['#eae8dd', '#4BBFC3', '#7BAABE', 'whitesmoke', '#f4f2f0'],
-        paddingTop: '3em',
-        paddingBottom: '10px',
-        fixedElements: '#header, .footer',
-        responsiveWidth: 0,
-        responsiveHeight: 0,
-        responsiveSlides: false,
-
-        //Custom selectors
-        sectionSelector: '.section',
-        slideSelector: '.slide',
-
-        //events
-        onLeave: function(index, nextIndex, direction){},
-        afterLoad: function(anchorLink, index){},
-        afterRender: function(){},
-        afterResize: function(){},
-        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
-    });
+//    if (screen && screen.width >= 800) {
+//        $('#fullpage').fullpage({
+//            menu: '#menu',
+//            lockAnchors: true,
+//            anchors:['Main', 'About', 'Events', 'Speakers', 'Sponsors', 'Location'],
+//            navigation: true,
+//            navigationPosition: 'right',
+//            showActiveTooltip: true,
+//            slidesNavigation: true,
+//            slidesNavPosition: 'bottom',
+//            fadingEffect:true,
+//            faddingEffect:'sections',
+//
+//            continuousHorizontal:true,
+//            scrollDelay: 2000,
+//            css3: true,
+//            scrollingSpeed: 900,
+//            autoScrolling: true,
+//            fitToSection: true,
+//            fitToSectionDelay: 4000,
+//            scrollBar: false,
+//            easing: 'easeInOutCubic',
+//            easingcss3: 'ease',
+//            loopBottom: false,
+//            loopTop: false,
+//            loopHorizontal: true,
+//            continuousVertical: false,
+//            continuousHorizontal: false,
+//            scrollHorizontally: false,
+//            interlockedSlides: false,
+//            resetSliders: false,
+//            normalScrollElements: '#element1, .element2',
+//            scrollOverflow: false,
+//            scrollOverflowOptions: null,
+//            touchSensitivity: 15,
+//            normalScrollElementTouchThreshold: 5,
+//            bigSectionsDestination: null,
+//
+//            keyboardScrolling: true,
+//            animateAnchor: true,
+//            recordHistory: true,
+//
+//            controlArrows: true,
+//            verticalCentered: true,
+//    //        sectionsColor : ['#eae8dd', '#4BBFC3', '#7BAABE', 'whitesmoke', '#f4f2f0'],
+//            paddingTop: '3em',
+//            paddingBottom: '10px',
+//            fixedElements: '#header, .footer',
+//            responsiveWidth: 0,
+//            responsiveHeight: 0,
+//            responsiveSlides: false,
+//
+//            sectionSelector: '.section',
+//            slideSelector: '.slide',
+//
+//            onLeave: function(index, nextIndex, direction){},
+//            afterLoad: function(anchorLink, index){},
+//            afterRender: function(){},
+//            afterResize: function(){},
+//            afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+//            onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+//        });
+//    };
+    
+    $('#menu li').click(function(){
+        $.fn.fullpage.moveTo($(this).attr('data-menuanchor'), 1);
+     });
     
     $( '#menu-btn' ).click(function(){
         $('.responsive-menu').toggleClass('expand');
     });
-    
-    $('#menu li').click(function(){
-         $.fn.fullpage.moveTo($(this).attr('data-menuanchor'), 1);
-     });
-    mapConfig();
 });
 
-   $("#lightSlider").lightSlider({
-        item:4,
-        loop:true,
-        slideMove:1,
-        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        speed:600,
-        responsive : [
-            {
-                breakpoint:800,
-                settings: {
-                    item:3,
-                    slideMove:1,
-                    slideMargin:6,
-                  }
-            },
-            {
-                breakpoint:480,
-                settings: {
-                    item:2,
-                    slideMove:1
-                  }
-            }
-        ]
-    });
-
-hs.graphicsDir = 'img/graphics/';
-hs.align = 'center';
-hs.transitions = ['expand', 'crossfade'];
-hs.wrapperClassName = 'dark borderless floating-caption';
-hs.fadeInOut = true;
-hs.dimmingOpacity = .75;
-
-// Add the controlbar
-if (hs.addSlideshow) hs.addSlideshow({
-    //slideshowGroup: 'group1',
-    interval: 5000,
-    repeat: false,
-    useControls: true,
-    fixedControls: 'fit',
-    overlayOptions: {
-        opacity: .6,
-        position: 'bottom center',
-        hideOnMouseOut: true
-    }
-});
-
+       
 var RequestData = function(type, URL, formData, callBack){
   // create a XHR object
   var xhr = new XMLHttpRequest();
