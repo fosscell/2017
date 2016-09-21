227aff454e15db5d71f07a600ebf5a790c21ee2d
Yash Agarwal <yashagarwaljpr@gmail.com>
2016-09-22 01:10:18 +0530

    adding animations

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ $("document").ready(function(){
         $(".preview img").attr("src", src);
     });
     
+    wow = new WOW({
+      boxClass: 'wow',      // default
+      animateClass: 'animated', // default
+      offset: 0,          // default
+      mobile: false,       // default
+      live: true        // default
+    });
+    
+    wow.init();
+    
     $('.set, .content, .grid')
         .jScrollPane({
             mouseWheelSpeed: 70,
