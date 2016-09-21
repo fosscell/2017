41e77591ecac053864e2171babbf7922f6e7e6ed
Yash Agarwal <yashagarwaljpr@gmail.com>
2016-09-21 16:28:06 +0530

    add new images and new gallery functionality

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 $("document").ready(function(){
     $('.hover img').mouseover(function() {
         var src = this.src;
-        console.log(src);
-        
         $(".preview img").attr("src", src);
     });
+    
+    $('.set')
+        .jScrollPane({
+            mouseWheelSpeed: 70,
+            autoReinitialise: true
+        })
+        .bind(
+            'mousewheel',
+            function(e) {
+                e.preventDefault();
+            }
+        );
 });
