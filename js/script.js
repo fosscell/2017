0ebc9869fc0ed350f6e26cfdf6d99bee151c9f8e
Yash Agarwal <yashagarwaljpr@gmail.com>
2016-09-22 18:10:14 +0530

    add responsiveness

--- js/script.js
+++ js/script.js
@@ -1,36 +1,95 @@
 $("document").ready(function(){
-    $('.hover img').mouseover(function() {
-        var src = this.src;
-        $(".preview img").attr("src", src);
-    });
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+                               
+    if (!isMobile.any()) {
+        $('.hover img').mouseover(function() {
+            var src = this.src;
+            $(".preview img").attr("src", src);
+        });
 
-    wow = new WOW({
-      boxClass: 'wow',      // default
-      animateClass: 'animated', // default
-      offset: 0,          // default
-      mobile: false,       // default
-      live: true        // default
-    });
+        wow = new WOW({
+          boxClass: 'wow',      // default
+          animateClass: 'animated', // default
+          offset: 0,          // default
+          mobile: false,       // default
+          live: true        // default
+        });
 
-    wow.init();
+        wow.init();
 
-    smoothScroll.init({
-        speed: 1500,
-        easing: 'easeInOutCubic',
-        updateURL: false,
-    });
+        smoothScroll.init({
+            speed: 1500,
+            easing: 'easeInOutCubic',
+            updateURL: false,
+        });
+        
+        $('.set, .content, .grid')
+            .jScrollPane({
+                mouseWheelSpeed: 70,
+                autoReinitialise: true
+            })
+            .bind(
+                'mousewheel',
+                function(e) {
+                    e.preventDefault();
+                }
+            );
+    } else {
+        $('.preview, .logo').remove();
     
-    $('.set, .content, .grid')
-        .jScrollPane({
-            mouseWheelSpeed: 70,
-            autoReinitialise: true
-        })
-        .bind(
-            'mousewheel',
-            function(e) {
-                e.preventDefault();
-            }
-        );
+        $('.filler').addClass('divider').removeClass('filler');
+
+        var array = ["FOSSMeet '16 Website", "Why Sponsor Us", "Previous Sponsors", "Reach Us"];
+
+        var j = 0;
+        $("#main-menu ul li").each(function(i) {
+            console.log(this.text());
+            $(this).text(array[j++]);
+        });
+    
+        $('#main-menu').smartmenus();
+
+        var $mainMenuState = $('#main-menu-state');
+        if ($mainMenuState.length) {
+            $mainMenuState.change(function(e) {
+                var $menu = $('#main-menu');
+                if (this.checked) {
+                    $menu.hide().slideDown(250, function() { 
+                        $menu.css('display', ''); 
+                    });
+                } else {
+                    $menu.show().slideUp(250, function() { 
+                        $menu.css('display', ''); 
+                    });
+                }
+            });
+                // hide mobile menu beforeunload
+            $(window).bind('beforeunload unload', function() {
+                if ($mainMenuState[0].checked) {
+                    $mainMenuState[0].click();
+                }
+            });
+        }
+    }
 });
 
 var DisplayMessageInModal = function(heading, message){
