e4b34174c0740ea00093712954366df78b4c0fdc
simsar <simsarulhaqv@gmail.com>
2016-10-20 21:25:57 +0530

    Update brochure download file name

--- js/script.js
+++ js/script.js
@@ -124,7 +124,7 @@ var DisplaySpecialLink = function(url, desc){
 };
 
 var PreviewURL = function(url){
-  DisplaySpecialLink("https://docs.google.com/gview?embedded=true&url=" + url, "<a href='" + url + "' style='text-decoration: none;' download='true'>Download Brochure</a>");
+  DisplaySpecialLink("https://docs.google.com/gview?embedded=true&url=" + url, "<a href='" + url + "' style='text-decoration: none;' download='FOSSMeet17.pdf'>Download Brochure</a>");
 };
 
 smoothScroll.init({
